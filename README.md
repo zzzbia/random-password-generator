a3b982821153c08d929044945e613b1be02169d5
R a b i a <rabia.saeed.1995@gmail.com>
2022-05-19 18:15:25 -0400

    added comments in js file and set validation for usableChars

--- README.md
+++ README.md
@@ -18,3 +18,10 @@
 ## Deployed Link
 
 ## References 📝
+
+-[W3 Schools Javascript Splitting](https://www.w3schools.com/jsref/jsref_split.asp)
+-[Javascript Array](https://sebhastian.com/javascript-array-string/)
+-[MDN Javascript Global Objects Math.Random](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random)
+-[CodeAcademy Forum Math.Random & MathFloor](https://www.codecademy.com/forum_questions/556f210b9113cbad080009bc)
+-[Freecodecamp Math.Random Method Explained](https://www.freecodecamp.org/news/javascript-math-random-method-explained/)
+-[Javascript Glossary](https://www.codecademy.com/article/glossary-javascript)
--- script.js
+++ script.js
@@ -1,34 +1,21 @@
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
-
-// Assignment code here
-
+//Setting strings of possible variable values
 const lowercase = "abcdefghijklmnopqrstuvwxyz";
 const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numeric = "0123456789";
 const special = "!@#$%^&*_-+=";
 
 const generatePassword = () => {
+	// usableChars is an empty string in which characters from the above variables will be passed through
 	let usableChars = "";
 
 	const passwordCharLength = prompt(
-		"Please enter how many characters you would like (min 8, max 128):",
+		"Please enter how many characters you would like your password to be, it must be between 8-128 characters long (min 8, max 128):",
 		8
 	);
-
+	/*if the password length is not a number OR 
+	password character length is less than 8 OR 
+	Password character length is more than 128 characters long, 
+	send an alert and exit out of the function "generatePassword" */
 	if (
 		isNaN(passwordCharLength) ||
 		passwordCharLength < 8 ||
@@ -37,59 +24,65 @@ const generatePassword = () => {
 		alert("Please enter a valid number between 8 and 128");
 		return;
 	}
+	/*otherwise, continue and confirm if user would like to use lowercase characters, 
+	 if yes then "+=" is used as concatenation/additon to set usable chars as now lowercase */
 	const useLowerCase = confirm("Would you like to use Lowercase characters?");
-
 	if (useLowerCase) {
 		usableChars += lowercase;
 	}
 
 	const useUpperCase = confirm("Would you like to use Uppercase characters?");
-
 	if (useUpperCase) {
 		usableChars += uppercase;
 	}
-	const useNumeric = confirm("Would you like to use numeric characters?");
 
+	const useNumeric = confirm("Would you like to use numeric characters?");
 	if (useNumeric) {
 		usableChars += numeric;
 	}
+
 	const useSpecialCharacters = confirm(
 		"Would you like to use special characters?"
 	);
-
 	if (useSpecialCharacters) {
 		usableChars += special;
 	}
-	console.log(usableChars);
+	//setting an empty array called "passwordArr"
+	let passwordArr = [];
+	// putting every character from string into an array with split method
+	let usableCharsArr = usableChars.split("");
+
+	if (usableCharsArr.length === 0) {
+		alert(
+			"Please select atleast one type of character (Lowercase, Uppercase, Numeric or Special characters.)"
+		);
+	}
+
+	for (i = 0; i < passwordCharLength; i++) {
+		const randomKey = Math.floor(Math.random() * usableCharsArr.length);
+		passwordArr.push(usableCharsArr[randomKey]);
+	}
+
+	const password = passwordArr.join("");
+	//
+	//Logging total Password length
+	//console.log("password length:" + password.length);
+	return password;
+
+	// When testing what characters are available, consolelog usableChars
+	//console.log(usableChars);
 };
-
-// const useLowerCase = confirm("Would you like to use Lowercase characters?");
-// const useUpperCase = confirm("Would you like to use Uppercase characters?");
-// const useSpecialCharacters = confirm("Would you like to use special characters?");
-//
-
-// const passwordLength = 8;
-// const useUpperCase = true;
-// const useLowerCase = false;
-// const useSpecialCharacters = false;
-
-// new Array(passwordLength).fill("").map((_ , i) => {
-
-// }).join('');
-
-//     console.log(`user wants ${passwordCharLength}`)
-//     console.log('hi')
-// }
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-	var password = generatePassword();
-	var passwordText = document.querySelector("#password");
-
-	passwordText.value = password;
+	const password = generatePassword();
+	if (password) {
+		const passwordText = document.querySelector("#password");
+		passwordText.value = password;
+	}
 }
 
 // Add event listener to generate button
